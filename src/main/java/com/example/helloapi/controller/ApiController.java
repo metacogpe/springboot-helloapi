fcf7a9662ea01c91e8f38e19b78843844c3988c1
metacog <metacog@kakao.com>
2022-05-28 13:28:05 +0900

    GetMapping-path

--- src/main/java/com/example/helloapi/controller/ApiController.java
+++ src/main/java/com/example/helloapi/controller/ApiController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController                     // 아래 클래스가 rest api 처리함을 알림
-@RequestMapping("/api")          // URL의 공통 부분
+@RequestMapping("/api")          // 주소 할당 : URL의 공통 부분
 public class ApiController {
 
-    @GetMapping("/hello")        // URL ; 동작 의미 : htp://localhost:9090/api/hello
+    @GetMapping("/hello")        // URL ; 동작 의미 : http://localhost:9090/api/hello
     public String hello() {
         return "hello SpringBoot!!!";
+    }
+
+    @GetMapping(path = "/hello-path")  // http://localhost:9090/api/get/hello-path
+    public String getHello() {
+        return "get Hello";
     }
 
     @GetMapping("/{name}")      // PathVariable 방식 : path명이 변하는 경우
